edb2b4f095664e8ef37ca72b4fc8bbf44039d647
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-02 18:01:44 +0200

    feat: added methods in GameUtils class

--- src/main/scala/it/unibo/pps/duckgame/utils/GameUtils.scala
+++ src/main/scala/it/unibo/pps/duckgame/utils/GameUtils.scala
@@ -1,14 +1,19 @@
 package it.unibo.pps.duckgame.utils
 
-import it.unibo.pps.duckgame.model.GameBoard
+import it.unibo.pps.duckgame.controller.GameStats
+import it.unibo.pps.duckgame.model.{GameBoard, Player}
+
+import scala.util.Random
 
 object GameUtils:
   
-  def addSumToPosition(sum: Int, position: Int, gameBoard: GameBoard): Int =
-    sum + position match
-      case result if result >= gameBoard.size => 
-        val updatedPosition = 63 - Math.abs(63 - result)
+  def addSumToPosition(sum: Int, position: Int): Int = sum + position match  
+    case result if result >= GameStats.gameBoard.size => val updatedPosition = 63 - Math.abs(63 - result)
         updatedPosition
-      case result => result
+    case result                                       => result
+      
+  def MixPlayers(players: List[Player]): List[Player] =
+    val mixedList = Random shuffle players
+    mixedList
       
   
