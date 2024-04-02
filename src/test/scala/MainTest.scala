a5e879cb74adacefa1358393c24a8fcbe9b0d7ba
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-02 17:59:40 +0200

    refactor: renamed enum for Cell types

--- src/test/scala/MainTest.scala
+++
@@ -1,5 +0,0 @@
-
-package it.unibo.pps.duckgame
-class MainTest {
-
-}
--- src/test/scala/it/unibo/pps/duckgame/BaseTest.scala
+++ src/test/scala/it/unibo/pps/duckgame/BaseTest.scala
@@ -1,13 +1,14 @@
-package it.unibo.pps.duckgame
-
-import it.unibo.pps.duckgame.model.{Game, Player}
-import org.junit.jupiter.api.Test
-
-@Test
-abstract class BaseTest:
-  var game: Game = new Game
-  var player: Player = new Player
-
-  def setUp(): Unit =
-    game = new Game
-    player = new Player
+//package it.unibo.pps.duckgame
+//
+//import it.unibo.pps.duckgame.controller.Game
+//import it.unibo.pps.duckgame.model.Player
+//import org.junit.jupiter.api.Test
+//
+//@Test
+//abstract class BaseTest:
+//  var game: Game = new Game
+//  var player: Player = new Player
+//
+//  def setUp(): Unit =
+//    game = new Game
+//    player = new Player
