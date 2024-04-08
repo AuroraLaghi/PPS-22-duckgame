f58f2366efd094bfd6d42462d9c3e0652b67d329
Aurora <aurora.laghi@studio.unibo.it>
2024-04-08 17:12:26 +0200

    refactor: update main class to start the game

--- src/main/scala/it/unibo/pps/duckgame/Main.scala
+++ src/main/scala/it/unibo/pps/duckgame/Main.scala
@@ -2,9 +2,12 @@ package it.unibo.pps.duckgame
 
 import it.unibo.pps.duckgame.controller.{Game, GameController}
 import it.unibo.pps.duckgame.model.Dice
+import it.unibo.pps.duckgame.utils.FxmlUtils.createPrimaryStage
 import it.unibo.pps.duckgame.view.CLI
+import scalafx.application.JFXApp3
 
-object Main:
+object Main extends JFXApp3:
   @main
-  def startGame(): Unit =
-    GameController.run()
+  override def start(): Unit =
+    //GameController.run()
+    createPrimaryStage()
