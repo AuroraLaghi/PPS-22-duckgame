3d4127872644941eebd99836424d7f4ee3287437
Aurora <aurora.laghi@studio.unibo.it>
2024-04-08 17:10:04 +0200

    chore: create files resources CssResources.scala and FxmlResources.scala and ImgResources.scala

---
+++ src/main/scala/it/unibo/pps/duckgame/utils/resources/CssResources.scala
@@ -0,0 +1,6 @@
+package it.unibo.pps.duckgame.utils.resources
+
+enum CssResources(_path: String):
+  case START_MENU_STYLE extends CssResources("/css/StartMenuStyle.css")
+  
+  val path: String = _path
