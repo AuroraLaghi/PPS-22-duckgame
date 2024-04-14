4a83283215fc9704465c384711d93e2b4de91f65
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-14 18:59:17 +0200

    feat: added new UI elements and implemented grid for gameboard

--- src/main/scala/it/unibo/pps/duckgame/Main.scala
+++ src/main/scala/it/unibo/pps/duckgame/Main.scala
@@ -1,7 +1,7 @@
 package it.unibo.pps.duckgame
 
 import it.unibo.pps.duckgame.controller.{Game, GameController}
-import it.unibo.pps.duckgame.model.Dice
+import it.unibo.pps.duckgame.model.{Dice, Player}
 import it.unibo.pps.duckgame.utils.FxmlUtils.createPrimaryStage
 import it.unibo.pps.duckgame.view.CLI
 import scalafx.application.JFXApp3
@@ -10,4 +10,9 @@ object Main extends JFXApp3:
   @main
   override def start(): Unit =
     //GameController.run()
+    val p1 = Player("p1")
+    val p2 = Player("p2")
+
+    GameController.addPlayer(p1)
+    GameController.addPlayer(p2)
     createPrimaryStage()
--- src/main/scala/it/unibo/pps/duckgame/view/GameBoardView.scala
+++ src/main/scala/it/unibo/pps/duckgame/view/GameBoardView.scala
@@ -1,26 +1,31 @@
 package it.unibo.pps.duckgame.view
 import it.unibo.pps.duckgame.controller.{GameController, GameStats}
 import it.unibo.pps.duckgame.model.Player
-import it.unibo.pps.duckgame.utils.FxmlUtils
+import it.unibo.pps.duckgame.utils.{FxmlUtils, GameUtils}
+import it.unibo.pps.duckgame.utils.resources.CssResources.START_MENU_STYLE
 import it.unibo.pps.duckgame.utils.resources.ImgResources
-import javafx.fxml
+import javafx.{fxml, geometry}
 import javafx.fxml.{FXML, Initializable}
 import javafx.geometry.Pos
 import javafx.scene.layout.{BorderPane, ColumnConstraints, GridPane, HBox, RowConstraints, VBox}
-import scalafx.stage.Screen
+import javafx.stage.Screen
 import javafx.fxml.{FXML, Initializable}
 import javafx.scene.control.{Button, Label, ListView, TableColumn, TableView}
 import javafx.scene.image.{Image, ImageView}
+import javafx.scene.layout
 
+import scala.collection.mutable.{ListBuffer, Map as MMap}
 import java.net.URL
 import java.util.ResourceBundle
+import scala.::
 
 class GameBoardView extends Initializable:
-  @FXML
-  private var playerList: VBox = _
+
+  private def N_COLS_IN_CELL = 4
+  private def N_ROWS_IN_CELL = 3
 
   @FXML
-  private var bottomMenu: VBox = _
+  private var actionsMenu: VBox = _
 
   @FXML
   private var turnLabel: Label = _
@@ -40,58 +45,99 @@ class GameBoardView extends Initializable:
   @FXML
   private var pane: BorderPane = _
 
-  private var playersHBox: Map[Player,HBox] = Map.empty
+  private var playersHBox: MMap[String,HBox] = MMap.empty
+
+  @FXML
+  private var playerListBox: VBox = _
 
   @FXML
   private var mainGrid: GridPane = _
 
+  private val cellsGrid: MMap[(Int, Int), GridPane] = MMap.empty
+
+  private val nameLabel: MMap[String, Label] = MMap.empty
+
   override def initialize(url: URL, resourceBundle: ResourceBundle): Unit =
-    val imageURL = new Image(getClass.getResource(ImgResources.GAMEBOARD.path).toString)
-    gameBoard.setImage(imageURL)
-    gameBoard.setPreserveRatio(false)
-    setResolution()
-    temp()
-
-  private def setResolution(): Unit =
-    FxmlUtils.setResolution(pane, 0.8, 0.8)
-    val(width,height) = FxmlUtils.getResolution
-
-    val gameBoardSize = pane.getPrefHeight
-
-    gameBoard.setFitWidth(gameBoardSize)
-    gameBoard.setFitHeight(gameBoardSize * 0.9)
-    val menuWidth = width - gameBoardSize
-    bottomMenu.setPrefWidth(menuWidth / 2)
-    playerList.setPrefWidth(menuWidth / 2)
-
-  private def temp(): Unit =
-    val p1 = Player("p1")
-    val p2 = Player("p2")
-
-    GameController.addPlayer(p1)
-    GameController.addPlayer(p2)
-    GameController.startGame()
+    FxmlUtils.initUIElements(pane, gameBoard, START_MENU_STYLE, FxmlUtils.DEFAULT_WIDTH_PERC,
+      FxmlUtils.DEFAULT_HEIGHT_PERC)
+    initializeCellGrid()
+    val menuWidth = FxmlUtils.getResolution._1 - pane.getPrefHeight
+    actionsMenu.setPrefWidth(menuWidth / 2)
+    playerListBox.setPrefWidth(menuWidth / 2)
+    GameStats.players.foreach(p => {
+      createPlayerBox(p)
+      nameLabel.addOne(p.name, new Label(p.name))
+      updatePlayerPosition(p)
+    })
 
   def quitButtonClick(): Unit =
-    playersHBox(GameStats.currentPlayer).setDisable(true)
+    nameLabel(GameStats.currentPlayer.name).setDisable(true)
+    playersHBox(GameStats.currentPlayer.name).setDisable(true)
     GameController.currentPlayerQuit()
+
+  private def canEndTurn(can: Boolean): Unit =
+    endTurnButton.setDisable(!can)
+    throwDiceButton.setDisable(can)
 
   def throwDiceButtonClick(): Unit =
     val (dice1, dice2) = GameController.throwDice()
     println("Dices: " + dice1.toString + " " + dice2.toString)
-    endTurnButton.setDisable(false)
-    throwDiceButton.setDisable(true)
+    updatePlayerPosition(GameStats.currentPlayer)
+    nameLabel.foreach(t =>
+      GameStats.players.find(p => p.name == t._1) match
+        case None => t._2.setDisable(true)
+        case _ =>
+    )
+    if dice1 != dice2 then
+      canEndTurn(true)
 
   def endTurnButtonClick(): Unit =
     GameController.endTurn()
-    endTurnButton.setDisable(true)
-    throwDiceButton.setDisable(false)
+    canEndTurn(false)
+
 
   private def createPlayerBox(player: Player): Unit =
+    val DEFAULT_SPACING = 10
     val playerHBox: HBox = new HBox()
-    playerList.getChildren.add(playerHBox)
+    playerListBox.getChildren.add(playerHBox)
     val playerLabel: Label = new Label(player.name)
     playerHBox.getChildren.add(playerLabel)
-    playerHBox.setSpacing(5)
-    playerHBox.setAlignment(Pos.CENTER)
-    playersHBox += (player -> playerHBox)
+    playerHBox.setSpacing(DEFAULT_SPACING)
+    playerHBox.setAlignment(geometry.Pos.CENTER)
+    playersHBox += (player.name -> playerHBox)
+
+  private def initializeCellGrid(): Unit =
+    val CONSTRAINT_PERC = 50
+    for
+      i <- 0 to GameUtils.CELLS_IN_SIDE
+      j <- 0 to GameUtils.CELLS_IN_SIDE
+    do
+      val tempGrid = new GridPane()
+      spawnColumns(tempGrid, N_COLS_IN_CELL)
+      spawnRows(tempGrid, N_ROWS_IN_CELL)
+
+      mainGrid.add(tempGrid, i, j)
+      cellsGrid.addOne((i, j), tempGrid)
+
+      def spawnColumns(grid: GridPane, numCol: Int): Unit =
+        val col = new ColumnConstraints()
+        col.setPercentWidth(CONSTRAINT_PERC)
+        for _ <- 0 until numCol
+        do
+          grid.getColumnConstraints.add(col)
+
+      def spawnRows(grid: GridPane, numRows: Int): Unit =
+        val row = new RowConstraints()
+        row.setPercentHeight(CONSTRAINT_PERC)
+        for _ <- 0 until numRows
+        do
+          grid.getRowConstraints.add(row)
+
+  private def updatePlayerPosition(player: Player): Unit =
+    val cellGrid = cellsGrid(GameUtils.getCoordinateFromPosition(player.actualPosition))
+    val (col, row) = getFirstFreeCellStartingFrom(cellGrid, cellGrid.getChildren.size(), (0, 1))
+
+    cellGrid.add(new Label(player.name), col, row)
+
+  private def getFirstFreeCellStartingFrom(gridPane: GridPane, nthCell: Int, startingCell: (Int, Int)): (Int, Int) =
+    GameUtils.getNthCellInGridWithStartingPos(nthCell + 1, (N_COLS_IN_CELL, N_ROWS_IN_CELL), startingCell)
