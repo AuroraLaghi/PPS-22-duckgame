92b14fdc60c52460a5adfce49b02f4ffefe4c178
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-15 15:58:35 +0200

    Revert "refactor: removed Test annotation"
    This reverts commit b1b0c12a4c268498eb2ee18459f2a8d950d2bb4b.

--- src/main/scala/it/unibo/pps/duckgame/controller/GameController.scala
+++ src/main/scala/it/unibo/pps/duckgame/controller/GameController.scala
@@ -6,57 +6,27 @@ import it.unibo.pps.duckgame.utils.GameUtils
 import scala.annotation.tailrec
 import scala.util.Try
 
-/**
- * Controller for the [[it.unibo.pps.duckgame.view.GameBoardView]]
- */
 object GameController:
-
-  /** Add a player to the players' list
-   * 
-   * @param player 
-   *  the player to be added
-   */
+  
   def addPlayer(player: Player): Unit =
     Game addPlayer player
 
-  /** Removes a player from the game
-   * 
-   * @param player
-   *  player entity to be removed
-   */
   def removePlayer(player: Player): Unit =
     Game removePlayer player
 
-  /**
-   * Initialize players' list mixing its values
-   */
   def startGame(): Unit =
     Game.players = GameUtils MixPlayers Game.players
 
-  /**
-   * Close the game
-   */
   def exitGame(): Unit =
     sys.exit(0)
 
-  /**
-   * Reset the game to the initial stats
-   */
   def newGame(): Unit =
     Game.reset()
 
-  /**
-   * Sets current player's new position
-   * @param steps
-   *  Number of cells to sum to old position
-   */
   def moveCurrentPlayer(steps: Int): Unit =
     PlayerController.updatePlayerWith(Game.currentPlayer, GameStats.currentPlayer.move(steps))
     if GameStats.checkVictory() then showVictory()
 
-  /**
-   * Called when a player quits the game
-   */
   def currentPlayerQuit(): Unit =
     val playerToDelete = GameStats.currentPlayer
     endTurn()
@@ -65,25 +35,14 @@ object GameController:
     Game.currentPlayer = Game.players.indexOf(nextPlayer)
     if GameStats.checkVictoryForSurrender() then showVictory()
 
-  /**
-   * Called when a player ends its turn
-   */
   def endTurn(): Unit =
     Game.currentPlayer = (Game.currentPlayer + 1) % Game.players.length
 
-  /** Method that throws two dices and return their result
-   * 
-   * @return
-   *  dices' values
-   */
   def throwDice(): (Int, Int) =
     val dicePair = Dice().roll()
     moveCurrentPlayer(dicePair._1 + dicePair._2)
     dicePair
 
-  /**
-   * Prints victory message with the name of the winner, then closes the application
-   */
   private def showVictory(): Unit =
     GameStats.winner.foreach(w =>
       println(s"${w.name} IS THE WINNER!\nGAME OVER!")
--- src/test/scala/it/unibo/pps/duckgame/model/PlayerTest.scala
+++ src/test/scala/it/unibo/pps/duckgame/model/PlayerTest.scala
@@ -1,9 +1,11 @@
 package it.unibo.pps.duckgame.model
 
+import org.junit.jupiter.api.{BeforeEach, Test}
 import org.scalatest.*
 import org.scalatest.flatspec.*
 import org.scalatest.matchers.*
 
+@Test
 class PlayerTest extends AnyFlatSpec with should.Matchers:
 
   val player: Player = Player()
