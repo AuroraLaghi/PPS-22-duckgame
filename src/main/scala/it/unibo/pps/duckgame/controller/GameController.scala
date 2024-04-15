31c1f191dac2da1a3a096cddaa27c8003b5fd7bf
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-15 15:16:33 +0200

    refactor: removed CLI class

--- src/main/scala/it/unibo/pps/duckgame/controller/GameController.scala
+++ src/main/scala/it/unibo/pps/duckgame/controller/GameController.scala
@@ -2,27 +2,17 @@ package it.unibo.pps.duckgame.controller
 
 import it.unibo.pps.duckgame.model.{Cell, CellStatus, Dice, GameBoard, Player}
 import it.unibo.pps.duckgame.utils.GameUtils
-import it.unibo.pps.duckgame.view.CLI
 
 import scala.annotation.tailrec
 import scala.util.Try
 
 object GameController:
-  private var _view: CLI = new CLI()
-
-  def view: CLI = _view
-  
-  def view_=(value: CLI): Unit = _view = value
   
   def addPlayer(player: Player): Unit =
     Game addPlayer player
-    
+
   def removePlayer(player: Player): Unit =
     Game removePlayer player
-    
-  def run(): Unit =
-    _view.showGameBoard()
-    _view.showGameStart()
 
   def startGame(): Unit =
     Game.players = GameUtils MixPlayers Game.players
@@ -35,8 +25,8 @@ object GameController:
 
   def moveCurrentPlayer(steps: Int): Unit =
     PlayerController.updatePlayerWith(Game.currentPlayer, GameStats.currentPlayer.move(steps))
-    if GameStats.checkVictory() then showVictory() 
-    
+    if GameStats.checkVictory() then showVictory()
+
   def currentPlayerQuit(): Unit =
     val playerToDelete = GameStats.currentPlayer
     endTurn()
@@ -44,22 +34,18 @@ object GameController:
     Game removePlayer playerToDelete
     Game.currentPlayer = Game.players.indexOf(nextPlayer)
     if GameStats.checkVictoryForSurrender() then showVictory()
-    
+
   def endTurn(): Unit =
     Game.currentPlayer = (Game.currentPlayer + 1) % Game.players.length
-    
+
   def throwDice(): (Int, Int) =
     val dicePair = Dice().roll()
     println("Dices: " + dicePair._1.toString + " " + dicePair._2.toString)
     moveCurrentPlayer(dicePair._1 + dicePair._2)
     dicePair
 
-  private def tryToInt(s: String) = Try(s.toInt).toOption
-  
   private def showVictory(): Unit =
-    GameStats.winner.foreach(w => 
+    GameStats.winner.foreach(w =>
       println(s"${w.name} IS THE WINNER!\nGAME OVER!")
-      exitGame())
-
-
-
+      exitGame()
+    )
