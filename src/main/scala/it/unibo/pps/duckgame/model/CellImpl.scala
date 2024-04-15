e3e84e614ba7880a41958d529672ad2c18a63a5c
Aurora <aurora.laghi@studio.unibo.it>
2024-04-15 16:31:43 +0200

    refactor: add java doc

--- src/main/scala/it/unibo/pps/duckgame/model/CellImpl.scala
+++ src/main/scala/it/unibo/pps/duckgame/model/CellImpl.scala
@@ -1,17 +1,12 @@
 package it.unibo.pps.duckgame.model
 
+/** Represents the cell class
+ *
+ * @param _number
+ * number
+ */
 class CellImpl(_number: Int) extends Cell:
   def number: Int = _number
-/** Implementation class of Cell
- *
- */
 object CellImpl:
-  /** Creates a cell with the given name
-   *
-   * @param number
-   *  number of the cell
-   * @return
-   *  the created cell
-   */
 
   def apply(number: Int): CellImpl = new CellImpl(number)
--- src/main/scala/it/unibo/pps/duckgame/model/Player.scala
+++ src/main/scala/it/unibo/pps/duckgame/model/Player.scala
@@ -2,10 +2,24 @@ package it.unibo.pps.duckgame.model
 
 import it.unibo.pps.duckgame.utils.GameUtils
 
+/** Represents a player in the game.
+ *
+ * @param actualPosition
+ *  The player's current position on the game board.
+ * @param name
+ *  The name of the player.
+ */
 final case class Player(
       actualPosition: Int,
       name: String):
-  
+
+  /** Moves the player by the specified number of steps.
+   *
+   * @param steps
+   *  The number of steps to move the player.
+   * @return
+   *  A new `Player` instance with the updated position.
+   */
   def move(steps: Int): Player =
     Player(GameUtils.addSumToPosition(steps, actualPosition), name)
     
