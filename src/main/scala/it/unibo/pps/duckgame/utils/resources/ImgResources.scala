bf73f614b1079e0064a5f4cc01c583c606ec6fa8
Aurora <aurora.laghi@studio.unibo.it>
2024-04-19 18:28:50 +0200

    refactor: added logo to start menu

--- src/main/scala/it/unibo/pps/duckgame/utils/resources/ImgResources.scala
+++ src/main/scala/it/unibo/pps/duckgame/utils/resources/ImgResources.scala
@@ -3,4 +3,6 @@ package it.unibo.pps.duckgame.utils.resources
 enum ImgResources(_path: String):
   case GAMEBOARD extends ImgResources("/img/gameboard.png")
   
+  case LOGO extends ImgResources("/img/logo.png")
+  
   val path: String = _path
--- src/main/scala/it/unibo/pps/duckgame/view/StartMenuView.scala
+++ src/main/scala/it/unibo/pps/duckgame/view/StartMenuView.scala
@@ -2,16 +2,19 @@ package it.unibo.pps.duckgame.view
 
 import it.unibo.pps.duckgame.controller.GameController
 import it.unibo.pps.duckgame.utils.FxmlUtils
-import it.unibo.pps.duckgame.utils.resources.{CssResources, FxmlResources}
+import it.unibo.pps.duckgame.utils.resources.{CssResources, FxmlResources, ImgResources}
 import javafx.fxml.{FXML, FXMLLoader, Initializable}
+import javafx.scene.image.Image
 import javafx.scene.layout.AnchorPane
 import javafx.{event as jfxe, fxml as jfxf, scene as jfxs}
 import javafx.scene.{control as jfxsc, layout as jfxsl}
 import scalafx.scene.Scene
+
 import java.net.URL
 import java.util.ResourceBundle
 import scalafx.Includes.*
 import scalafx.scene.control.Button
+import javafx.scene.image.ImageView
 import scalafx.stage.Screen
 
 class StartMenuView extends Initializable:
@@ -28,12 +31,18 @@ class StartMenuView extends Initializable:
   @SuppressWarnings(Array("org.wartremover.warts.Null", "org.wartremover.warts.Var"))
   private var startMenuPane: AnchorPane = _
 
+  @FXML
+  private var logo: ImageView = _
+
   override def initialize(url: URL, resourceBundle: ResourceBundle): Unit =
     startMenuPane.getStylesheets.add(getClass.getResource(CssResources.GAME_STYLE.path).toExternalForm)
     FxmlUtils.setPaneResolution(startMenuPane, 0.6, 0.6)
+    logo.setImage(new Image(getClass.getResource(ImgResources.LOGO.path).toString))
 
   def playGame(): Unit =
     FxmlUtils.changeScene(FxmlResources.PLAYER_MENU.path)
 
   def exitGame(): Unit =
     GameController.exitGame()
+
+
