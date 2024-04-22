d4f7c56523dafcb705b0ada007e58c98eb34e561
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-22 16:10:02 +0200

    feat: created test class for Cell entity

---
+++ src/test/scala/it/unibo/pps/duckgame/model/CellTest.scala
@@ -0,0 +1,25 @@
+package it.unibo.pps.duckgame.model
+
+import it.unibo.pps.duckgame.controller.GameReader
+import it.unibo.pps.duckgame.model.specialCell.{SpecialCell, SpecialCellType}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class CellTest extends AnyFlatSpec with should.Matchers:
+  private def SKELETON_CELL = 58
+  private def STARTING_CELL = 0
+  private def FINAL_CELL = 63
+
+  "Skeleton's special cell in the gameboard cells's list" should "have index 58" in {
+    GameReader.gameBoard.gameBoardList(SKELETON_CELL) shouldBe a [SpecialCell]
+  }
+
+  "Standard cells" should "be returned as they are" in {
+    GameReader.gameBoard.gameBoardList(STARTING_CELL) shouldBe a [Cell]
+  }
+
+  "GameBoard special cells list" should "contain correct cells' types" in {
+    GameReader.gameBoard.specialCells.find(_.number == FINAL_CELL).orNull.specialCellType shouldBe SpecialCellType.FINAL
+  }
+
+
