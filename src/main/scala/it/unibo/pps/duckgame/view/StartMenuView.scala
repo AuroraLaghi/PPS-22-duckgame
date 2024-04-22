ebb21684035d0de4b8ab2043a0e153c548b86627
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-22 09:34:19 +0200

    refactor: updated FXML file for StartMenu

--- src/main/scala/it/unibo/pps/duckgame/view/StartMenuView.scala
+++ src/main/scala/it/unibo/pps/duckgame/view/StartMenuView.scala
@@ -1,11 +1,12 @@
 package it.unibo.pps.duckgame.view
 
-import it.unibo.pps.duckgame.controller.GameController
+import it.unibo.pps.duckgame.controller.StartMenuController
 import it.unibo.pps.duckgame.utils.FxmlUtils
+import it.unibo.pps.duckgame.utils.resources.CssResources.GAME_STYLE
 import it.unibo.pps.duckgame.utils.resources.{CssResources, FxmlResources, ImgResources}
 import javafx.fxml.{FXML, FXMLLoader, Initializable}
 import javafx.scene.image.Image
-import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.{AnchorPane, BorderPane}
 import javafx.{event as jfxe, fxml as jfxf, scene as jfxs}
 import javafx.scene.{control as jfxsc, layout as jfxsl}
 import scalafx.scene.Scene
@@ -18,6 +19,13 @@ import javafx.scene.image.ImageView
 import scalafx.stage.Screen
 
 class StartMenuView extends Initializable:
+  
+  private def WIDTH = 0.4
+  private def HEIGHT = 0.4
+
+  @FXML
+  @SuppressWarnings(Array("org.wartremover.warts.Null", "org.wartremover.warts.Var"))
+  private var pane: BorderPane = _
 
   @FXML
   @SuppressWarnings(Array("org.wartremover.warts.Null", "org.wartremover.warts.Var"))
@@ -28,21 +36,18 @@ class StartMenuView extends Initializable:
   private var exitButton: Button = _
 
   @FXML
-  @SuppressWarnings(Array("org.wartremover.warts.Null", "org.wartremover.warts.Var"))
-  private var startMenuPane: AnchorPane = _
-
-  @FXML
   private var logo: ImageView = _
 
   override def initialize(url: URL, resourceBundle: ResourceBundle): Unit =
-    startMenuPane.getStylesheets.add(getClass.getResource(CssResources.GAME_STYLE.path).toExternalForm)
-    FxmlUtils.setPaneResolution(startMenuPane, 0.6, 0.6)
+    FxmlUtils.initUIElements(pane, GAME_STYLE, WIDTH, HEIGHT)
     logo.setImage(new Image(getClass.getResource(ImgResources.LOGO.path).toString))
+    logo.setFitWidth(FxmlUtils.getResolution._1 * WIDTH)
+    logo.setFitHeight(FxmlUtils.getResolution._2 * HEIGHT)
 
   def playGame(): Unit =
-    FxmlUtils.changeScene(FxmlResources.PLAYER_MENU.path)
+    StartMenuController.playGame()
 
   def exitGame(): Unit =
-    GameController.exitGame()
+    StartMenuController.closeGame()
 
 
