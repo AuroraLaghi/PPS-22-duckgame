126dbff9d9c3badf2350dc98190dea7160e36076
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-22 09:33:43 +0200

    feat: implemented controller for StartMenuView

---
+++ src/main/scala/it/unibo/pps/duckgame/controller/StartMenuController.scala
@@ -0,0 +1,22 @@
+package it.unibo.pps.duckgame.controller
+
+import it.unibo.pps.duckgame.utils.FxmlUtils
+import it.unibo.pps.duckgame.utils.resources.FxmlResources
+
+/**
+ * Controller for the [[it.unibo.pps.duckgame.view.StartMenuView]]
+ */
+object StartMenuController:
+
+  /**
+   * Called when PLAY button is pressed
+   */
+  def playGame(): Unit =
+    LogicController.startGame()
+    FxmlUtils.changeScene(FxmlResources.PLAYER_MENU.path)
+
+  /**
+   * Called when EXIT button is pressed
+   */
+  def closeGame(): Unit =
+    LogicController.exitGame()  
