e5203d65ddf8f06ed02bcc8ca405eef9f1eaedea
Aurora <aurora.laghi@studio.unibo.it>
2024-04-29 17:17:33 +0200

    refactor: game board now creates cells list by reading json file

--- src/main/scala/it/unibo/pps/duckgame/model/GameBoard.scala
+++ src/main/scala/it/unibo/pps/duckgame/model/GameBoard.scala
@@ -2,6 +2,9 @@ package it.unibo.pps.duckgame.model
 
 import it.unibo.pps.duckgame.model.specialCell.SpecialCellType.DUCK
 import it.unibo.pps.duckgame.model.specialCell.{SpecialCell, SpecialCellBuilder, SpecialCellType}
+import it.unibo.pps.duckgame.utils.JsonUtils
+import it.unibo.pps.duckgame.utils.deserialization.SpecialCellDeserializer
+import it.unibo.pps.duckgame.utils.resources.JsonResources
 import scalafx.scene.control
 
 /** Represents the game board
@@ -32,41 +35,7 @@ class GameBoard(
 
 object GameBoard:
   def apply(): GameBoard =
-    var duckNumbers = List(5, 9, 14, 18, 23, 27, 32, 36, 41, 45, 50, 54, 59)
-    val normalCells = List(
-      0 to 4,
-      7 to 8,
-      10 to 13,
-      15 to 17,
-      20 to 22,
-      24 to 26,
-      28 to 30,
-      33 to 35,
-      37 to 40,
-      43 to 44,
-      46 to 49,
-      51,
-      53,
-      55 to 57,
-      60 to 62
-    )
-    var listNormal = normalCells.flatMap {
-      case range: Range => range.toList
-      case number: Int => List(number)
-    }
-    var specialCells = List(
-      SpecialCellBuilder(6, SpecialCellType.BRIDGE, "").build(),
-      SpecialCellBuilder(31, SpecialCellType.WELL, "").build(),
-      SpecialCellBuilder(42, SpecialCellType.LABYRINTH, "").build(),
-      SpecialCellBuilder(58, SpecialCellType.SKELETON, "").build(),
-      SpecialCellBuilder(52, SpecialCellType.JAIL, "").build(),
-      SpecialCellBuilder(19, SpecialCellType.HOUSE, "").build(),
-      SpecialCellBuilder(63, SpecialCellType.FINAL, "").build()
-    )
-    val duckList = duckNumbers.map(n => SpecialCellBuilder(n, SpecialCellType.DUCK, "").build())
-    specialCells = specialCells++duckList
-    val numbers: List[Cell] = listNormal.map(n => CellImpl(n))++specialCells
-    new GameBoard(numbers, specialCells)
+    new GameBoard((0 to 63).map(n => CellImpl(n)).toList, JsonUtils.readCellsType[SpecialCell](JsonResources.SPECIAL_CELLS, SpecialCellDeserializer ))
 
   def apply(cells: List[Cell], specialCell: List[SpecialCell]): GameBoard =
     new GameBoard(cells, specialCell)
