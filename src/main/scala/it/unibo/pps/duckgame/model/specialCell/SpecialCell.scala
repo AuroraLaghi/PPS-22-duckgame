905b5bb8bbc00198347682bfc4732db3b53014df
ffrattini <francesca.frattini7@studio.unibo.it>
2024-04-29 14:46:43 +0200

    refactor: added message attribute in SpecialCell class

--- src/main/scala/it/unibo/pps/duckgame/model/specialCell/SpecialCell.scala
+++ src/main/scala/it/unibo/pps/duckgame/model/specialCell/SpecialCell.scala
@@ -5,9 +5,11 @@ import it.unibo.pps.duckgame.model.{Cell, CellStatus, Player}
 class SpecialCell(
     override val number: Int,
     val specialCellType: SpecialCellType,
+    val message: String,
     val action: Int => Unit
 ) extends Cell:
 
   object SpecialCell:
-    def apply(number: Int, specialCellType: SpecialCellType, action: Int => Unit): SpecialCell =
-      new SpecialCell(number, specialCellType, action)
+    def apply(number: Int, specialCellType: SpecialCellType, message: String, action: Int => Unit): SpecialCell =
+      new SpecialCell(number, specialCellType, message, action)
+
